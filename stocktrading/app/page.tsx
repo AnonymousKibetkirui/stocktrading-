2cf0f5045bdced271b51dbbde32bfd3d60739894
AnonymousKibetkirui <kibetfestus666@gmail.com>
2026-05-13 08:16:26 +0300

    initial commit

---
+++ stocktrading/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+const Home :() =>Element = () => {
+  return (
+   <div className="flex justify-center  items-center h-screen ">
+        <Button> Start Trading</Button>
+    </div>
+    
+  )
+} 
+
+export default  Home 
